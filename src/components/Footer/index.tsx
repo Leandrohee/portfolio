733f45d71aafc77e1f6e4d73a61ed83acb8489bb
leandroTorres <leandrohenrique_@live.com>
2025-02-14 19:28:00 -0300

    feat: terminei o footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom"
+import { Link} from "react-router-dom"
 import styled from "styled-components"
 import sun from "../../assets/sunWhite.png"
+import moon from "../../assets/moonWhite.png"
 import {motion}  from "framer-motion"
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 /* --------------------------------------------- CSS -------------------------------------------- */
@@ -62,31 +63,74 @@ export const FooterSc = styled.footer`
     }
 
     .themePicker{
-        background-color: #0dc04f4a;
+        /* background-color: #0dc04f4a; */
         display: flex;
-        align-items: center;
+        align-items: end;
         justify-content: space-around;
         height: 40%;
         width: 100%;
         padding: 10px;
+        cursor: pointer;
+        padding-bottom: 50px;
+        margin-top: 10vh;
     }
 `
 
-const Stick = styled.div`
-    width: 2px;
-    height: 30px;
-    background-color: #000;
-    /* margin: 0 25px; */
-    /* transform: scaley(2); */
+const Stick = styled.div<StickProps>`
+    width: 100%;
+    height: 60px;
+    /* background-color: #ffffff48; */
+    display: flex;
+    justify-content: start;
+    align-items: end;
+    
+    .stick-contant{
+        height: ${({$isSelected,$isSibling})=>(
+            $isSelected 
+                ? '50px' 
+                : $isSibling 
+                ? '30px'
+                : '20px'
+        )};
+        margin-bottom: ${({$isSelected, $isSibling})=>(
+                $isSelected 
+                ? '10px' 
+                : $isSibling 
+                ? '5px'
+                : ''
+        )};
+        width: 2px;
+        background-color: ${({$dayHour})=>(
+            $dayHour >= 6 && $dayHour <= 18 
+                ? "#FEFAE0" : "#000"
+        )};
+        pointer-events: none;
+    }
+
+    &:hover{
+        .stick-contant{
+            height: ${({$isSelected})=>($isSelected ? '50px' : '45px')};
+            transition: height .2s;
+            margin-bottom: 10px;
+        }
+    }
+
 `
 
 const Sun = styled(motion.img)`
-    position: absolute;
+    position: relative;
     background-color: #0A2E36;
     padding: 11px;
-    height: 45px;
+    height: 50px;
     border-radius: 50%;
 `
+/* ----------------------------------------- INTERFACES ----------------------------------------- */
+interface StickProps{
+    $isSelected?: boolean;
+    $isSibling?: boolean;
+    $dayHour: number;
+}
+
 
 /* ------------------------------------- LINKS IN THE FOOTER ------------------------------------ */
 const githubLink = "https://github.com/Leandrohee"
@@ -98,23 +142,40 @@ const contactLink = "/contact"
 /* ------------------------------------------ COMPONENT ----------------------------------------- */
 export default function Footer() {
 
-    const [stickSelected, setStickSelect] = useState(0)
-    const [positionIcon, setPositionIcon] = useState()
+    const [stickSelected, setStickSelect] = useState<number>(12)
+    const [positionIcon, setPositionIcon] = useState<(number)[]>([])
     const stickRef = useRef<(HTMLDivElement|null)[]>([])
+    const themeDiv = useRef<(HTMLDivElement|null)[]>([])
 
+    //Cada elemento dessa array representa 1 hora do dia
     const arr = Array.from({ length: 24 }, (_, index) => index);
 
+    //Carregando a posicao ao entrar na pagina
+    useEffect(()=>{
+        getStickPosition()
+    },[])
 
-    function getStickPosition(){
-        // let currentStick = stickRef.current.find(stick => (
-        //     stick?.getAttribute("data-key") == "0"
-        // ))
+    //Mudando a posicao do stick
+    function getStickPosition(element?: HTMLDivElement){
+        let currerrentStick = stickRef.current[12]
+        element 
+            ? currerrentStick = element 
+            : currerrentStick = stickRef.current[12]
 
-        let currerrentStick = stickRef.current[stickSelected]
+        const leftPosition = currerrentStick?.offsetLeft;
+        const topPosition = currerrentStick?.offsetTop;
+        let width = currerrentStick?.offsetWidth ?? 0
+        if(width) {width =  width/2}
+        const divPosition = themeDiv.current[0]?.offsetLeft
+
+        setPositionIcon([leftPosition ?? 0, topPosition ?? 0, width, divPosition ?? 0])
     }
 
-    function teste (){
-        console.log(stickRef)
+    //Clicando em algum stick
+    function handleClick(event: React.MouseEvent<HTMLDivElement>){
+        const target = event.target as HTMLDivElement
+        setStickSelect(Number(target.dataset.key) ?? 0)
+        getStickPosition(target)
     }
 
     return (
@@ -136,19 +197,42 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="themePicker" onClick={teste}> 
-                <Sun
-                    initial={{ left: 0, top: 0}}
-                    src={sun}
-                />
-
+            <div 
+                className="themePicker" 
+                ref={(element)=> (themeDiv.current[0] = element)} >                     {/*Descrobrindo a distancia da div pai dos sticks  */}
+                { positionIcon[0] &&
+                    <Sun
+                        initial={{ 
+                            left: positionIcon[0] - positionIcon[3] - 30,               //Esse 30 é +- a width da imagem sun ou moon
+                            top: -50
+                        }}
+                        animate={{ 
+                            left: positionIcon[0] - positionIcon[3] - 30, 
+                            top: -50,
+                            opacity: [0,0,1],
+                            transition:{
+                                duration:  0.2
+                            }
+                        }}
+                        src={stickSelected > 18 || stickSelected < 6 ? moon : sun}
+                    />
+                } 
 
                 {arr.map(item => (
-                    <Stick 
+                    <Stick
+                        $isSelected={stickSelected == item ? true : false}
+                        $isSibling={
+                            stickSelected == item + 1 || stickSelected ==  item - 1
+                            ? true : false
+                        }
+                        $dayHour={item}
                         key={item}
                         data-key={item}
                         ref= {(element) => (stickRef.current[item] = element)}
-                    />
+                        onClick={handleClick}
+                    >
+                        <div className="stick-contant"></div>
+                    </Stick>
                 ))}
             </div>  
         </FooterSc>
